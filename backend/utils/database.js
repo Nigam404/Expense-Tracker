aaf12cb942199f846a4fbc263e7abc8cda714951
Nigam <nigamkalpataru@gmail.com>
2024-01-14 13:14:58 +0530

    making code production ready

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
+const dotenv = require("dotenv").config();
 
-const sequelize = new Sequelize("nigamdb", "root", "Nigam@12345", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
